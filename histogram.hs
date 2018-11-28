e13270193cf51867e63e6c3e6859438f410ea3a0
JaineyJames <james.jaineyelsa@gmail.com>
2018-11-28 05:46:22 -0500

    Map version

--- histogram.hs
+++ histogram.hs
@@ -1,7 +1,9 @@
 import           Data.Char          (isAlpha, isPunctuation, toLower)
 import           Data.Hashable      (hash)
-import qualified Data.HashMap       as HM
+-- import qualified Data.HashMap       as HM
 import           Data.List
+import           Data.Map.Strict    (Map)
+import qualified Data.Map.Strict    as Map
 import           Data.Typeable      (typeOf)
 import           System.Environment (getArgs)
 
@@ -17,10 +19,10 @@ processFile str = let
       in [trimmed | word <- chunks lower, let trimmed = trim word, trimmed /= ""]
 
 
-insertion :: [String] -> HM.Map String a -> HM.Map String a
-insertion (x:xs) countMap = if HM.member x countMap
-                                then HM.alter (+1) x countMap
-                                else HM.insert x 1 countMap
+insertion :: [String] -> Map k a -> Map k a
+insertion (x:xs) countMap = if Map.member (hash x) countMap
+                                then Map.alter (+1) (hash x) countMap
+                                else Map.insert (hash x) 1 countMap
 
 hashWords :: String -> Int
 hashWords s = hash s
@@ -34,6 +36,6 @@ main = do
            then print "Usage: File"
            else do
                   fileContent <- readFile (args!!0)
-                  let countMap = HM.empty
+                  let countMap = Map.empty
                   insertion (processFile fileContent) countMap
                   print (processFile fileContent)
